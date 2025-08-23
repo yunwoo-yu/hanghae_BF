4930f491cd9743b03f33b3c489119eb4363380d7
soyalattee <uyyu1734@gmail.com>
2025-08-23 20:23:44 +0900

    feat: baseAPI 세팅

---
+++ src/apis/baseApi.ts
@@ -0,0 +1,53 @@
+import axios, { type AxiosRequestConfig } from "axios";
+
+// 환경별 API baseURL 설정
+const API_BASE_URL = "/api";
+
+const axiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const http = {
+  get: async <Response = unknown>(
+    url: string,
+    options: AxiosRequestConfig = {}
+  ) => {
+    const response = await axiosInstance.get<Response>(url, options);
+    return response.data;
+  },
+  post: async <Request = unknown, Response = unknown>(
+    url: string,
+    data?: Request,
+    options?: AxiosRequestConfig
+  ) => {
+    const response = await axiosInstance.post<Response>(url, data, options);
+    return response.data;
+  },
+  put: async <Request = unknown, Response = unknown>(
+    url: string,
+    data?: Request
+  ) => {
+    const response = await axiosInstance.put<Response>(url, data);
+    return response.data;
+  },
+  patch: async <Request = unknown, Response = unknown>(
+    url: string,
+    data?: Request
+  ) => {
+    const response = await axiosInstance.patch<Response>(url, data);
+    return response.data;
+  },
+  delete: async <Response = unknown>(url: string) => {
+    const response = await axiosInstance.delete<Response>(url);
+    return response.data;
+  },
+  download: async <Response = Blob>(url: string) => {
+    const response = await axiosInstance.get<Response>(url, {
+      responseType: "blob",
+    });
+    return response.data;
+  },
+};
