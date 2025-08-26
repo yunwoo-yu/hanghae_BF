7b5eb3de7deaf5dcc5a95274407378d3e42c5e73
jeongmingi123 <60804074+jeongmingi123@users.noreply.github.com>
2025-08-27 00:35:38 +0900

    feat : home page Login 컴포넌트 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,25 @@
-import { MatchingDialog } from '@/components/MatchingDialog';
+import { useState } from 'react';
+
+import Login from '@/components/Login';
+import { SplashScreen } from '@/components/SplashScreen';
 
 export const Home = () => {
+  const [showSplash, setShowSplash] = useState(true);
+
+  const handleSplashComplete = () => {
+    setShowSplash(false);
+  };
+
+  if (showSplash) {
+    return <SplashScreen onComplete={handleSplashComplete} duration={4000} isSkipButton={true} />;
+  }
+
   return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <h1 className="text-3xl font-bold text-blue-400">2팀 짱짱</h1>
+    // This div will be the full-width wrapper inside the centered body.
+    <div className="inset-0 fixed">
+      <div className="flex h-screen items-center justify-center">
+        <Login />
+      </div>
     </div>
   );
 };
