1b8097fda8468b257a40314c88753b1677f401da
bebusl <bebus1998@naver.com>
2025-08-30 18:56:39 +0900

    🔧 chore: QueryClient 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { Routers } from './routers/router';
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <Routers />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Routers />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
